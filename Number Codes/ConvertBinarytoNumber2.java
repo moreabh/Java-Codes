b37ff4bddb0dc34109ff90d78cc7060e09333693
moreabh <137516580+moreabh@users.noreply.github.com>
2025-02-25 14:37:24 +0530

    20/2 number program

---
+++ Number Codes/ConvertBinarytoNumber2.java
@@ -0,0 +1,14 @@
+//logic 2 : Convert Binary To number 
+import java.util.*;
+class ConvertBinarytoNumber2
+{
+	public static void main(String[]args)
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a number : ");
+		int num = sc.nextInt();
+		String bin = Integer.toBinaryString(num);
+		System.out.println(num+":" +bin);
+
+	}
+}
---
+++ Number Codes/FacinatingNoWithoutString.java
@@ -0,0 +1,36 @@
+//facinating number without string
+class FacinatingNoWithoutString
+{
+	public static void main(String[]args)
+	{
+		int num = 327;
+		int dup =num;
+		boolean flag = true;
+		for (int i=2;i<=3 ;i++ )
+		{
+			num = (num*1000)+(dup*i);
+		}
+			for (int i=1;i<=9 ;i++ )
+			{
+				int cnt = 0;
+				for (int j=num;j>0 ;j/=10 )
+				{
+					int rem = j%10;
+					if (i==rem)
+					{
+						cnt++;
+
+					}
+				}
+				if (cnt!=1)
+				{
+					flag=false;
+					break;
+				}
+			}
+			System.out.println(flag ? dup +" is a facinating Number"
+										: dup+" is not Facinating Number");
+								
+		
+	}
+}
