405572017f3f88b632b1a9865355924d69058573
moreabh <137516580+moreabh@users.noreply.github.com>
2025-03-18 19:54:18 +0530

    commit

--- Number Codes/UniqueNo.java
+++ Number Codes/UniqueNo.java
@@ -8,20 +8,20 @@ class UniqueNo
 	public static boolean isUnique(int num)
 	{
 		//int dup=num;//123
-		int rem2=0;
-		int rem=0;
+		//int rem2=0;
+		//int rem=0;
 		for (int i=num;i>0;num/=10)                  //123
 		{    
-			System.out.println("num:"+num);
-			 rem = i%10;  //3
+			//System.out.println("num:"+num);
+			int rem = i%10;  //3
 			 i/=10;
-			 System.out.println("rem:"+rem);//3
+			// System.out.println("rem:"+rem);//3
 			 
 
 			for (int j=i;j>0;j/=10)
 			{
-				 rem2 = j%10;
-				 System.out.println("rem2="+rem2);//2 1 3  
+				int rem2 = j%10;
+				// System.out.println("rem2="+rem2);//2 1 3  
 				  if (rem==rem2)                  
 				  {
 					 System.out.println(rem+":"+rem2+" Not a Unique Number");//
@@ -34,6 +34,7 @@ class UniqueNo
 	}
 }
 /*
+num:512347546
 rem:6
 rem2=4
 rem2=5
@@ -50,4 +51,46 @@ rem2=7
 rem2=4
 4:4 Not a Unique Number
 false
+*/
+
+
+
+/*
+num:1237546
+rem:6
+rem2=4
+rem2=5
+rem2=7
+rem2=3
+rem2=2
+rem2=1
+num:123754
+rem:4
+rem2=5
+rem2=7
+rem2=3
+rem2=2
+rem2=1
+num:12375
+rem:5
+rem2=7
+rem2=3
+rem2=2
+rem2=1
+num:1237
+rem:7
+rem2=3
+rem2=2
+rem2=1
+num:123
+rem:3
+rem2=2
+rem2=1
+num:12
+rem:2
+rem2=1
+num:1
+rem:1
+It is a unique Number
+true
 */
--- Number Codes/UniqueNo2.java
+++ Number Codes/UniqueNo2.java
@@ -3,22 +3,24 @@ class UniqueNo2
 {
 	public static void main(String[]args)
 	{
-		System.out.println(isUnique(5123546));
+		System.out.println(isUnique(123546));
 	}
 	public static int isUnique(int num)
 	{
 		int cnt=0;
+		System.out.println("num:"+num);
 		for (int i=num;i>0;num/=10 )
 		{
 			int rem1 = num%10;
 			i/=10;
-			System.out.println("i="+i);
+			
+			//System.out.println("i="+i);
 			for (int j=i;j>0;j/=10)
 			{
 				int rem2 = j%10;
 				if (rem1==rem2)
 				{
-					System.out.println(+rem1+":"+rem2);
+					System.out.println(rem1+":"+rem2);
 					cnt++;
 					System.out.println("cnt:"+cnt);
 					System.out.println("Is not unique Number");
